99c50e8939f087b22f3e9c76cc0901d97c84f13c
Garance <vallat.garance@etu.unice.fr>
2015-05-11 15:01:49 +0200

    begin slider

--- app/scripts/controllers/pictures.js
+++ app/scripts/controllers/pictures.js
@@ -10,4 +10,13 @@
 angular.module('restaurantApp')
   .controller('PicturesCtrl', ['$scope', function ($scope) {
     $scope.Pictures = [];
+
+
+      angular.element(document).ready(function(){
+        angular.element('.bxslider').bxSlider({
+          mode : 'fade',
+          auto:true,
+          adaptiveHeight: true
+        });
+      });
   }]);
