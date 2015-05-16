389bcf9e2ba78c89b47f8e6422fc6097da93b3f8
Garance <vallat.garance@etu.unice.fr>
2015-05-16 18:10:32 +0200

    menus ok !!

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -9,43 +9,16 @@
  */
 angular.module('restaurantApp')
     .controller('MenuCtrl', ['$scope', '$window', '$location', 'Menu', 'Course', function ($scope, $window, $location, Menu, Course) {
-  /*    $scope.deleteU = function() {
-        User.delete($scope.theUId,
-            function(error) {
-              console.log(error);
-            });
-        $window.location.reload();
-      };
 
-      $scope.getAUser = function (theIdILookFor) {
-        //    get: function (userId, successCB, failCB) { //successCB & fail sont des fonctions qu'on appelle avec des params
-        User.get(theIdILookFor,
-            function (result) {
-              $scope.theUserToUpgrade = result;
+        Course.all(function (courses) { // là on appelle le service : ne renvoie rien en soit
+                $scope.theCourses = courses; //on met dans une var globale au scope le résultat de la fonction
             },
+            //en 2è arg, la fonction quand c'est un échec
             function (error) {
-              $scope.theUserToUpgrade = 'rate!'; //must deal w/ error
+                $scope.theCourses=[];
+                console.log(error); //ici normalement, besoin de gérer les erreurs :
+                //par ex on remplit un $scope.error où on donne d'autres infos
             });
-      };
-
-
-      $scope.updateUser = function () {
-        // update: function (userId, newData, successCB, failCB) {
-        User.update($scope.theUserToUpgrade.id, $scope.theUserToUpgrade,
-            function (result) {
-              console.log('made it!\n');
-              console.log('the result :  ' + result+ '\n');
-              $scope.theUserToUpgrade = result;
-            },
-            function (error) {
-              console.log(error);
-              $scope.theUserToUpgrade = 'rate!';
-            });
-
-        console.log('result : ' + $scope.theUserToUpgrade+'\n');
-        $window.location.reload();
-      };
-*/
 
       //en premier arg, ma fonction quand c'est un succes
       Menu.all(function (menus) { // là on appelle le service : ne renvoie rien en soit
@@ -71,14 +44,20 @@ angular.module('restaurantApp')
       $scope.goToComments = function (theMenuIdYouWant) {
             $location.path("/com/"+theMenuIdYouWant);
       };
+
+        $scope.setCourseIdIWant = function(thatCourse) {
+            $scope.courseIWant = thatCourse;
+        };
         //getMenuCourses
-        $scope.getThisCourse = function(courseIdIWant) {
-            Course.get(courseIdIWant,
+         $scope.getTheCourse = function (theIdYouAskedFor) {
+            Course.get(theIdYouAskedFor,
                 function (result) {
-                    $scope.MyCourseIWanted = result;
+                    $scope.thatCourse = result;
+                    console.log(thatCourse);
                 },
                 function (error) {
-                    $scope.MyCourseIWanted = 0; //must deal w/ error
+                    $scope.thatCourse = []; //must deal w/ error
+                    console.log("error");
                 });
         }
     }
