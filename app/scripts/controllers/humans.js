5769c3985a5e9f39972a501c0ce7c567a84e60de
Garance <vallat.garance@etu.unice.fr>
2015-05-19 19:07:32 +0200

    meilleurs menus

--- app/scripts/controllers/humans.js
+++ app/scripts/controllers/humans.js
@@ -8,7 +8,7 @@
  * Controller of the restaurantApp
  */
 angular.module('restaurantApp')
-  .controller('HumansCtrl', ['$scope', 'Human', function ($scope, Human) {
+  .controller('HumansCtrl', ['$scope', 'Human', 'Course', function ($scope, Human, Course) {
       Human.all(function (humans) { // là on appelle le service : ne renvoie rien en soit
             $scope.theHumans = humans; //on met dans une var globale au scope le résultat de la fonction
           },
@@ -22,5 +22,16 @@ angular.module('restaurantApp')
       $scope.theBosses = [];
       $scope.theCooks = [];
       $scope.theWaiters = [];
+
+
+        Course.all(function (courses) { // là on appelle le service : ne renvoie rien en soit
+                $scope.theCourses = courses; //on met dans une var globale au scope le résultat de la fonction
+            },
+            //en 2è arg, la fonction quand c'est un échec
+            function (error) {
+                $scope.theCourses=[];
+                console.log(error); //ici normalement, besoin de gérer les erreurs :
+                //par ex on remplit un $scope.error où on donne d'autres infos
+            });
 
     }]);
